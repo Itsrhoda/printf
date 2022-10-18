04ab3ad8ad0511a75e6bad19b9cea1c9a579a8ae
mary-muthoni <kamaum772@gmail.com>
2022-10-18 01:18:38 -0700

    task 0

---
+++ 1_printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * @ _you don’t have to handle the flag chararacers
+ * @wid: you don’t have to handle field width
+ * @pre: You don’t have to handle precision
+ * @mod: You don’t have to handle the length modifiers
