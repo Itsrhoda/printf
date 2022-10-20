a410e4d519c85af511a7779d46008fdc9cc504c0
Itsrhoda” <rodahmanyaga.com>
2022-10-20 12:41:10 -0700

    2-number

---
+++ 2-printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_hex - prits unsigned hex numbers in lowercase
+ * @ap: the argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
+int print_hex(va_list ap, params_t *params)
+{
+	unsigned long l;
+	int c = 0;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
